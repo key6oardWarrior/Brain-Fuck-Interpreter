a8df6ca276b666d6e8390690cc8353d1a4f1b05f
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2021-04-12 13:02:49 -0400

    Add files via upload

---
+++ StaticHeaders/Environment.h
@@ -0,0 +1,61 @@
+#pragma once
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+
+class Environment {
+private:
+    std::vector<char> memoryPointer;
+    std::vector<char>::iterator mp;
+
+public:
+    Environment() : memoryPointer(3000) {
+        mp = memoryPointer.begin();
+    }
+
+public:
+    /*
+    * @return mp
+    */
+    std::vector<char>::iterator getMP(void) const;
+
+    /*
+    * @return the end of memoryPointer
+    */
+    std::vector<char>::const_iterator endMemPtr(void) const;
+
+    /*
+    * @return the begining of memoryPointer
+    */
+    std::vector<char>::const_iterator beginMemPtr(void) const;
+
+    /*
+    * increment the mp pointer
+    */
+    inline void incMP(void) { this->mp++; }
+
+    /*
+    * decrement the mp pointer
+    */
+    inline void decMP(void) { this->mp--; }
+
+    /*
+    * @param userLetter - set user input to *mp
+    */
+    inline void userInput(const char& userLetter) { *(this->mp) = userLetter; }
+
+    /*
+    * add 0x1 to *mp
+    */
+    inline void increment(void) { *(this->mp) += 0x1; }
+
+    /*
+    * subtract 0x1 from *mp
+    */
+    inline void decrement(void) { *(this->mp) -= 0x1; }
+
+    /*
+    * @return size of 
+    */
+};
+
+#endif // !ENVIRONMENT_H
