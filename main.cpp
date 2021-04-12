9f6a571931cf8d04f0c929c3f83db5daae17ec4c
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2021-04-12 13:20:02 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+
+
+int main(void) {
+    std::string fileName = "";
+
+    std::cout << "What is the location and name of your file? ";
+    std::getline(std::cin, fileName);
+
+    Interpreter* bF = new Interpreter(fileName);
+    bF->main();
+
+    delete bF;
+    bF = nullptr;
+
+    system("pause");
+    return 0;
+}
---
+++ pch.h
@@ -0,0 +1,20 @@
+// pch.h: This is a precompiled header file.
+// Files listed below are compiled only once, improving build performance for future builds.
+// This also affects IntelliSense performance, including code completion and many code browsing features.
+// However, files listed here are ALL re-compiled if any one of them is updated between builds.
+// Do not add files here that you will be updating frequently as this negates the performance advantage.
+
+#pragma once
+#ifndef PCH_H
+#define PCH_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include "StaticHeaders\Errors.h"
+#include "StaticHeaders\Environment.h"
+#include "Headers\BrainFuckInterpreter.h"
+
+#endif // PCH_H
