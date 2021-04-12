89d14eac8857e64481ac0d69b642f1d50212ed2b
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2021-04-12 13:02:09 -0400

    Add files via upload

---
+++ Errors.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef ERRORS_H
+#define ERRORS_H
+
+/**
+* determin what error message to display
+*/
+enum class ErrorMessage {
+    noError = 0,
+    codeNotOpen = 1,
+    invalidSymbol = 2,
+    unmatchedBrackets = 3,
+    stackOverFlow = 4
+};
+
+#endif // !ERRORS_H
