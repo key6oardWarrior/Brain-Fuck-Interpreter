f4567bfef53dcb57e86cddf64c1ec3febf713614
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2021-04-28 03:12:24 -0400

    removed useless spaces

--- Environment.cpp
+++ Environment.cpp
@@ -1,13 +1,13 @@
 #include "pch.h"
 
 std::vector<char>::iterator Environment::getMP(void) const {
-				return mp;
+	return mp;
 }
 
 std::vector<char>::const_iterator Environment::endMemPtr(void) const {
-				return memoryPointer.end()--;
+	return memoryPointer.end()--;
 }
 
 std::vector<char>::const_iterator Environment::beginMemPtr(void) const {
-				return memoryPointer.begin();
+	return memoryPointer.begin();
 }
