dcb39f75245e842b09e86d99fa2d375270858248
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2021-05-29 15:19:38 -0400

    Ensure that users can only use .bf files insead of .txt or something else

--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@ int main(void) {
     std::cout << "What is the location and name of your file? ";
     std::getline(std::cin, fileName);
 
+    if(fileName.substr(fileName.length()-3) != ".bf") {
+        std::cout << "Error:" << std::endl;
+        std::cout << "code exited with error code 6: File must be a brain fuck (.bf) file." << std::endl;
+        return 0;
+    }
+
     Interpreter* bF = new Interpreter(fileName);
     bF->main();
 
