0c5f06eab324e13c3029d14cf039496e6a47ed51
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2023-01-20 16:28:53 -0500

    Update Environment.cpp

--- Environment.cpp
+++ Environment.cpp
@@ -5,14 +5,6 @@ unsigned short Environment::getMemIdx(void) const {
 	return memoryIndex;
 }
 
-unsigned short Environment::getCopyMemory(void) {
-	return memory[memoryIndex];
-}
-
-unsigned short& Environment::getRefMemory(void) {
-	return memory[memoryIndex];
-}
-
 unsigned short const& Environment::getConstMemory(void) {
 	return memory[memoryIndex];
 }
