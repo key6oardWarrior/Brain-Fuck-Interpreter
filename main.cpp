8938141b3d9e6cc91fb0808f3d78744c371843ca
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2023-01-20 16:22:36 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include "pch.h"
 #include "Headers\BrainfuckInterpreter.hpp"
 
-int main(int argc, char** argv) {
+int main(const int argc, const char** argv) {
+	if(argc != 2) {
+		std::cout << "Error:\n";
+		std::cout << "code exited with code 7: Expected 1 command line argument, but got " << argc-1;
+		return 0;
+	}
+
 	const std::string fileName = argv[1];
 
 	if(fileName.substr(fileName.length() - 3) != ".bf") {
