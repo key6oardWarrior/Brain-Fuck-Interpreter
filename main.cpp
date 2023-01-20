3b3e0f8ff9a9723b0fe47e800e85edd5ede1a6da
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2023-01-20 16:18:43 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,21 @@
 #include "pch.h"
 #include "Headers\BrainfuckInterpreter.hpp"
 
-int main(void) {
-    std::string fileName = "";
+int main(int argc, char** argv) {
+	const std::string fileName = argv[1];
 
-    std::cout << "What is the location and name of your file? ";
-    std::getline(std::cin, fileName);
+	if(fileName.substr(fileName.length() - 3) != ".bf") {
+		std::cout << "Error:" << std::endl;
+		std::cout << "code exited with error code 6: File must be a brain fuck (.bf) file." << std::endl;
+		return 0;
+	}
 
-    if(fileName.substr(fileName.length()-3) != ".bf") {
-        std::cout << "Error:" << std::endl;
-        std::cout << "code exited with error code 6: File must be a Brainfuck (.bf) file." << std::endl;
-        return 0;
-    }
+	Interpreter* bf = new Interpreter(fileName);
+	bf->main();
 
-    Interpreter* bf = new Interpreter(fileName);
-    bf->main();
+	delete bf;
+	bf = nullptr;
 
-    delete bf;
-    bf = nullptr;
-
-    system("pause");
-    return 0;
+	system("pause");
+	return 0;
 }
